a0f3fcc781c9694de9d29eda8a8b2756b4b68cc8
jrmarcco <jrmarcco@gmail.com>
2025-08-11 16:29:12 +0800

    chore: tmp code.

--- internal/service/callback/callback_service.go
+++ internal/service/callback/callback_service.go
@@ -1,6 +1,13 @@
 package callback
 
-import "context"
+import (
+	"context"
+
+	"github.com/JrMarcco/easy-kit/xsync"
+	"github.com/JrMarcco/kuryr/internal/domain"
+	"github.com/JrMarcco/kuryr/internal/repository"
+	"go.uber.org/zap"
+)
 
 type Service interface {
 	SendCallback(ctx context.Context, startTime int64, batchSize int) error
@@ -8,7 +15,17 @@ type Service interface {
 
 var _ Service = (*DefaultService)(nil)
 
-type DefaultService struct{}
+type DefaultService struct {
+	callbackConfigMap xsync.Map[uint64, *domain.CallbackConfig] // biz id -> callback config
+
+	// TODO: 范型 client manager
+	// grpcClinets *client.Manager[callbackv1.CallbackServiceClient]
+
+	logRepo       repository.CallbackLogRepo
+	bizConfigRepo repository.BizConfigRepo
+
+	logger *zap.Logger
+}
 
 func (s *DefaultService) SendCallback(ctx context.Context, startTime int64, batchSize int) error {
 	// TODO: implement me
