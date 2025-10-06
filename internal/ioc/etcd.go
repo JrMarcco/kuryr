77fc15640995929f93b91ce30f92d08858d45cb7
jrmarcco <jrmarcco@gmail.com>
2025-10-06 20:54:32 +0800

    fix: etcd tls config.

--- internal/ioc/etcd.go
+++ internal/ioc/etcd.go
@@ -52,15 +52,6 @@ func InitEtcdClient(logger *zap.Logger, lc fx.Lifecycle) *clientv3.Client {
 		tlsCfg := &tls.Config{
 			MinVersion:         tls.VersionTLS13,
 			InsecureSkipVerify: cfg.TLS.InsecureSkipVerify,
-			CipherSuites: []uint16{
-				// 支持现代密码套件，包括 Ed25519
-				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			},
 		}
 		if cfg.TLS.ServerName != "" {
 			tlsCfg.ServerName = cfg.TLS.ServerName
